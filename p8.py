91467bcfad4bbf6081f68212b0ca6929a6c5b2e7
Spandan Dutta <67653554+spandy07@users.noreply.github.com>
2020-11-13 00:32:25 +0530

    Update p8.py

--- p8.py
+++ p8.py
@@ -1,3 +1,5 @@
+# A timestamp is three numbers: a number of hours, minutes and seconds. Given two timestamps, calculate how many seconds is between them.
+# The moment of the first timestamp occurred before the moment of the second timestamp.
 hrs_1 = int(input("Hour1: "))
 min_1 = int(input("Minute1: "))
 sec_1 = int(input("Second1: "))
