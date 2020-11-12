4f28631c529d441d164ecc11d4c082d623798664
Spandan Dutta <67653554+spandy07@users.noreply.github.com>
2020-11-13 00:29:41 +0530

    Update p5.py

--- p5.py
+++ p5.py
@@ -1,3 +1,4 @@
+# Write a program that reads the length of the base and the height of a right-angled triangle and prints the area. Every number is given on a separate line.
 length = 4
 height = 5
 area = (length*height)/2
