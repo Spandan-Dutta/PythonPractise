03e6979a6bda44ff8a04767bb9c199de8ec0a646
Spandan Dutta <67653554+spandy07@users.noreply.github.com>
2020-11-13 00:31:33 +0530

    Update p7.py

--- p7.py
+++ p7.py
@@ -1,3 +1,6 @@
+# Write a program that reads an integer number and prints its previous and next numbers. See the examples below for the exact format your answers should take. 
+# There shouldn't be a space before the period.
+# Remember that you can convert the numbers to strings using the function str.
 n = int(input("Enter a number: "))
 print("The next number for the number is n is: " , n+1)
 print("The previous number for the number is n is: " , n-1)
